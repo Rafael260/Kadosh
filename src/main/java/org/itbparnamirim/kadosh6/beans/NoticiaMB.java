f9ee96ad2877d7d1c45f00adc7ccf96a06c3485a
Geraldogrj <geraldogrj@gmail.com>
2017-06-03 23:33:36 -0300

    Modelagem da classe de domínio Notícias

---
+++ src/main/java/org/itbparnamirim/kadosh6/beans/NoticiaMB.java
@@ -0,0 +1,16 @@
+package org.itbparnamirim.kadosh6.beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Geraldo Jr
+ */
+@ManagedBean
+@SessionScoped
+public class NoticiaMB implements Serializable {
+    
+    
+}
---
+++ src/main/java/org/itbparnamirim/kadosh6/data/NoticiaDAO.java
@@ -0,0 +1,18 @@
+package org.itbparnamirim.kadosh6.data;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Geraldo Jr
+ */
+@Named
+@RequestScoped
+public class NoticiaDAO extends TemplateDAO {
+    
+    
+    
+    
+    
+}
