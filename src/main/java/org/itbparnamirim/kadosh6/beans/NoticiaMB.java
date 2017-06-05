1b2faa5bd39b6cdae8bb3d298d750cd3aa5eec84
Geraldogrj <geraldogrj@gmail.com>
2017-06-05 20:27:36 -0300

    Classe notícia

--- src/main/java/org/itbparnamirim/kadosh6/beans/NoticiaMB.java
+++ src/main/java/org/itbparnamirim/kadosh6/beans/NoticiaMB.java
@@ -1,16 +1,98 @@
 package org.itbparnamirim.kadosh6.beans;
 
 import java.io.Serializable;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import org.itbparnamirim.kadosh6.data.NoticiaDAO;
+import org.itbparnamirim.kadosh6.model.Noticia;
 
 /**
  *
  * @author Geraldo Jr
  */
-@ManagedBean
 @SessionScoped
+@Named
 public class NoticiaMB implements Serializable {
+
+    private Noticia noticia = new Noticia();
+    private List<Noticia> noticias = new ArrayList<>();
+
+    @Inject
+    NoticiaDAO noticiaDAO;
+
+    public Noticia getNoticia() {
+        return noticia;
+    }
+
+    public void setNoticia(Noticia noticia) {
+        this.noticia = noticia;
+    }
+
+    public List<Noticia> getNoticias() {
+        return noticias;
+    }
+
+    public void setNoticias(List<Noticia> noticias) {
+        this.noticias = noticias;
+    }
+
+    public String salvar() {
+        try {
+            noticia = noticiaDAO.save(noticia);
+            limparObjetos();
+        } catch (IllegalStateException ex) {
+            Logger.getLogger(NoticiaMB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(NoticiaMB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SystemException ex) {
+            Logger.getLogger(NoticiaMB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "/pages/noticias/exibirNoticia" + ManagedBeanUtil.REDIRECT;
+    }
+
+    private void limparObjetos() {
+        this.noticia = new Noticia();
+    }
     
+    public String deletar (Noticia noticia){
+        String pagDestino = "/pages/noticias/exibirNoticia.xhtml";
+        try {
+            noticiaDAO.delete(noticia);
+            ManagedBeanUtil.refresh();
+        } catch (IllegalStateException | SecurityException | SystemException | RollbackException | HeuristicMixedException | HeuristicRollbackException ex) {
+            Logger.getLogger(NoticiaMB.class.getName()).log(Level.SEVERE, null, ex);
+            pagDestino = "/pages/dashboardAdmin.xhtml";
+        }
+        return pagDestino + ManagedBeanUtil.REDIRECT;
+    }
     
+    public String prepararEdicao(Noticia noticia){
+        this.noticia = noticia;
+        return "/pages/noticias/cadastrarNoticia.xhtml" + ManagedBeanUtil.REDIRECT;
+    }
+    
+    public String prepararConsulta(Noticia noticia){
+        this.noticia = noticia;
+        return "/pages/noticias/detalharNoticia.xhtml" + ManagedBeanUtil.REDIRECT;
+    }
+    
+    public String prepararCadastro(Noticia noticia){
+        limparObjetos();
+        return "/pages/noticias/cadastrarNoticia.xhtml"+ManagedBeanUtil.REDIRECT;
+    }
+    
+    public String publicarNoticia(Noticia noticia){
+        this.noticia = noticia;
+        return "/pages/noticias/publicarNoticia"+ManagedBeanUtil.REDIRECT;
+    }
+
 }
--- src/main/java/org/itbparnamirim/kadosh6/data/NoticiaDAO.java
+++ src/main/java/org/itbparnamirim/kadosh6/data/NoticiaDAO.java
@@ -1,7 +1,14 @@
 package org.itbparnamirim.kadosh6.data;
 
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.TypedQuery;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import org.itbparnamirim.kadosh6.model.Noticia;
 
 /**
  *
@@ -10,9 +17,39 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class NoticiaDAO extends TemplateDAO {
+
+    public NoticiaDAO() {
+    }
     
+    public Noticia save(Noticia noticia) throws IllegalStateException, SecurityException, SystemException{
+        try {
+            userTransaction.begin();
+            if (noticia.getId() == null){
+                em.persist(noticia);
+            } else {
+                em.merge(noticia);
+            }
+            userTransaction.commit();
+        } catch (Exception e) {
+            userTransaction.rollback();
+        }
+        return noticia;
+    }
     
+    public List<Noticia> list(){
+        TypedQuery<Noticia> query = em.createNamedQuery("findAllNoticias", Noticia.class);
+        return query.getResultList();
+    }
     
+    public void delete(Noticia noticia) throws IllegalStateException, SecurityException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException{
+        try {
+            userTransaction.begin();
+            em.remove(noticia);
+            userTransaction.commit();
+        } catch (Exception e) {
+            userTransaction.rollback();
+        }
+    }
     
     
 }
